bf82a4bb8be7decff5285145893a93a183eb79f5
Patrycja Zawadka <metapeach@icloud.com>
2022-08-11 14:57:46 -0700

    Add deploying scripts

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # sumcheck protocol
 
 
+# Deploy Verifier smart contract
+Inside `vierfier` directory please run following scripts:
+```
+npm run test
+npm run build
+npm run deploy
+```
+After deploying copy contract address to your .env file.
+
--- verifier/truffle-config.js
+++ verifier/truffle-config.js
@@ -1,4 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const rpcURL = process.env.RPC_URL;
+const mnemonic = process.env.WALLET_MNEMONIC;
+
 module.exports = {
+  networks: {
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, rpcURL),
+      network_id: "5",
+    },
+    local: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      websockets: true
+    },
+  },
   compilers: {
       solc: {
         version: "^0.8.0"
